b5cd4f0a424cb9db7a2b741d1b2cae289aedee4e
Tung <s3794510@rmit.edu.vn>
2023-09-14 16:12:41 +0700

    modify to match the HTTP response

--- lambda_function.js
+++ lambda_function.js
@@ -13,10 +13,16 @@ exports.lambda_handler = async (event) => {
         const pem = jwkToPem(jwks.keys[0]); // Convert the JWK to PEM format
         const decodedToken = jwt.verify(token, pem, { algorithms: ['RS256'] });
 
-        return { userId: decodedToken.sub };
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ userId: decodedToken.sub })
+        };
     } catch (err) {
         console.error('Failed to decode or verify token:', err);
-        throw new Error('Invalid token or unauthorized');
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ error: 'Invalid token or unauthorized' })
+        };
     }
 };
 
