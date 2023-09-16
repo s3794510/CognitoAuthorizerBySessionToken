62484cb538d9dbc6f7054255483fb9ea69882677
Tung <s3794510@rmit.edu.vn>
2023-09-16 22:16:28 +0700

    change request handling to include header

--- lambda_function.js
+++ lambda_function.js
@@ -6,8 +6,32 @@ const COGNITO_POOL_URL = process.env.COGNITO_POOL_URL;
 const COGNITO_POOL_PUBLIC_KEY_URL = `${COGNITO_POOL_URL}/.well-known/jwks.json`;
 
 exports.lambda_handler = async (event) => {
-    const token = event.userToken;  // Extracting token from event.userToken
+    const authHeader = event.headers.Authorization || event.headers.authorization; // Headers may be case-sensitive
 
+    if (!authHeader) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ message: 'Authorization header is missing' })
+        };
+    }   
+
+    // Extract the token from the Authorization header
+    const token = authHeader.split(' ')[1]; // Assumes "Bearer YOUR_TOKEN_HERE" format
+
+    // Now, you can use the token for your logic, like verifying it
+    //console.log('Extracted Token:', token);
+    
+    // Check if the token is blank, null, or undefined
+    // if (!token || token.trim() === '' || typeof token === 'undefined') {
+    //     return {
+    //         statusCode: 401,
+    //         body: JSON.stringify({ 
+    //             error: 'Token is blank, null, or undefined',
+    //             token: token
+    //         })
+    //     };
+    // }
+    
     try {
         const jwks = await getJWKS();
         const pem = jwkToPem(jwks.keys[0]); // Convert the JWK to PEM format
@@ -15,13 +39,16 @@ exports.lambda_handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ userId: decodedToken.sub })
+            body: JSON.stringify(decodedToken)
         };
     } catch (err) {
         console.error('Failed to decode or verify token:', err);
         return {
             statusCode: 401,
-            body: JSON.stringify({ error: 'Invalid token or unauthorized' })
+            body: JSON.stringify({ 
+                error: 'Invalid token or unauthorized',
+                token: token  // Adding the token to the error response
+            })
         };
     }
 };
